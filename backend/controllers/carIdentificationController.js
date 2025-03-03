24961aece1b37e19b04e40cd0fb4fa3bba457c8c
abdsham92 <134995411+abdsham92@users.noreply.github.com>
2025-03-03 15:03:47 +0100

    updated the post request controller to prevent duplicate car collections

--- backend/controllers/carIdentificationController.js
+++ backend/controllers/carIdentificationController.js
@@ -1,16 +1,16 @@
-import makeModel, { makeModelQuery } from "../models/makeModel.js"
+import carModels, { carModelsQuery } from "../models/carModelsSchema.js"
 
 // @desc Retrieve Car Identification
 // @route GET /api/carIdentification
 // @access Public
 export const getCarIdentification = async (req, res) => {
-  // ?type=query (make, model, etc..)
+  // ?type=query
   const { type } = req.query
   console.log(`A GET request has been made using the query: ${type}`)
-  
-  const result = await makeModel.findOne()
-  console.log(`The imported query is ${makeModelQuery}`)
-  if (type && result && type === makeModelQuery) {  
+
+  const result = await carModels.findOne()
+
+  if (type && result && type === carModelsQuery) {
     res.status(201).json({
       auto: result,
       message: "Found!",
@@ -27,23 +27,35 @@ export const getCarIdentification = async (req, res) => {
 // @route POST /api/carIdentification
 // @access Public
 export const postCarIdentification = async (req, res) => {
-  const { nextProperty, options } = req.body;
-  console.log(nextProperty, options)
-
-  if (nextProperty && options.length > 0) {
-    await makeModel.create({
-      nextProperty,
-      options,
-    })
-    res.status(200).json({
-      Status: "Valid Request!",
-      nextProperty,
-      options,
-    })
-  } else {
-    res.status(400).json({
-      status: "400",
-      message: "Bad Request",
+  try {
+    const { _id, nextProperty, options } = req.body
+    if (!nextProperty || !options.length) {
+      return res.status(400).json({
+        status: "400",
+        message: "Bad Request",
+      })
+    }
+    if (nextProperty && options.length > 0) {
+      await carModels.create({ _id, nextProperty, options })
+      res.status(200).json({
+        Status: "Valid Request!",
+        _id,
+        nextProperty,
+        options,
+      })
+    }
+  } catch (error) {
+    if (error.message.includes("duplicate key error")) {
+      return res.status(409).json({
+        status: "409",
+        message:
+          "Duplicate entry. This record already exists. Change _id to add a new record",
+      })
+    }
+    res.status(500).json({
+      status: "500",
+      message: "Internal Server Error",
+      error: error.message,
     })
   }
 }
