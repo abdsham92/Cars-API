1bf8b4ee8daa9faf6d06a59dad10850d7c408f9c
abdsham92 <134995411+abdsham92@users.noreply.github.com>
2025-03-04 21:02:54 +0100

    finalized the main route controller with the response logic to be sent to the UI

--- backend/controllers/carIdentificationController.js
+++ backend/controllers/carIdentificationController.js
@@ -1,28 +1,79 @@
-import carModels from "../models/carModelsSchema.js"
+import carInfo from "../models/carInfoSchema.js"
+import dotenv from "dotenv"
+
+// load environment variables
+dotenv.config()
+const protocol = process.env.PROTOCOL
+const host = process.env.SERVER_HOST
+const port = process.env.PORT
 
 // @desc Retrieve Car Identification
-// @route GET /api/carIdentification
+// @route POST /api/carIdentification
 // @access Public
 const getCarIdentification = async (req, res) => {
   // define allowed queries
-  const allowedQueries = [
-    "make",
-    "model",
-    "registrationMonth",
-    "registrationYear",
-  ]
+  const callRoute = "/api/carModels"
+  const callQuery = "make"
+  // this should resolve to http://localhost:5000/api/carModels?type=make
+  const API_URL = `${protocol}://${host}:${port}${callRoute}?type=${callQuery}`
 
-  // ?type=query
-  const { type } = req.query
-  console.log(`A GET request has been made using the query: ${type}`)
+  let availableModelsResult = await fetch(API_URL)
+  availableModelsResult = await availableModelsResult.json()
 
-  const result = await carModels.findOne()
+  const allowedQueries = availableModelsResult.options.map(
+    (option) => option.value
+  )
 
-  if (type && result && allowedQueries.includes(type)) {
-    res.status(201).json({
-      auto: result,
-      message: "Found!",
-    })
+  // ?model=query
+  const { model } = req.query
+  if (!allowedQueries.includes(model)) {
+    return res.status(404).json({ message: "Bad request! Model not found!" })
+  } else {
+    console.log(`A GET request has been made using the query: ${model}`)
+  }
+
+  // make a call to the database get the available models of the queried car brand
+  const modelResult = await carInfo.findOne({
+    nextProperty: "registrationMonth",
+    model: [model],
+  })
+  if (!modelResult) {
+    return res.status(404).json({ message: "Bad request! No Model Results!" })
+  }
+
+  // make a call to the database get the available manufacturing months of the queried car brand
+  const registrationMonthResult = await carInfo.findOne({
+    nextProperty: "registrationYear",
+    model: [model],
+  })
+  if (!registrationMonthResult) {
+    return res
+      .status(404)
+      .json({ message: "Bad request! Missing Registration Month!" })
+  }
+
+  // make a call to the database get the available manufacturing years of the queried car brand
+  //TODO
+  const registrationYearResult = await carInfo.findOne({
+    nextProperty: null,
+    model: [model],
+  })
+  if (!registrationYearResult) {
+    return res
+      .status(404)
+      .json({ message: "Bad request! Missing registration Year!" })
+  }
+
+  if (
+    model &&
+    modelResult &&
+    registrationMonthResult &&
+    registrationYearResult &&
+    allowedQueries.includes(model)
+  ) {
+    res
+      .status(201)
+      .json([modelResult, registrationMonthResult, registrationYearResult])
   } else {
     res.status(400).json({
       status: "400",
