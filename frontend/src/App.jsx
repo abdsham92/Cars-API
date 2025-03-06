8a7a7de766d9b86805a03a88ab755546e82635ff
abdsham92 <134995411+abdsham92@users.noreply.github.com>
2025-03-06 15:03:56 +0100

    added comments to the App.jsx to make it more clear

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -90,32 +90,41 @@ function App() {
 
 // ---------- useEffects/functions/methods... END -----------------------------------------------------------------------------
 
-  // Find registration month and year data for the selected brand
-  
+//----------Find models, registration months, and years data for the currently selected brand------------START--------------------------
+
+  // isolate the models from the car data json
   const availableModelData = carDetails.find(
     (item) => item.model === selectedBrand && item.nextProperty === "registrationMonth"
   );
+  // isolate the model options from the models json
+  const availableModels = availableModelData ? availableModelData.options : [];
 
+  // isolate the months from the car data json
   const registrationMonthData = carDetails.find(
     (item) => item.model === selectedBrand && item.nextProperty === "registrationYear"
   );
+  // isolate the month options from the months json
+  const registrationMonths = registrationMonthData ? registrationMonthData.options : [];
 
+  // isolate the years from the car data json
   const registrationYearData = carDetails.find(
     (item) => item.model === selectedBrand && item.nextProperty == null
   );
-
-  const availableModels = availableModelData ? availableModelData.options : [];
-
-  const registrationMonths = registrationMonthData ? registrationMonthData.options : [];
-
+  // isolate the years options from the years json
   const registrationYears = registrationYearData ? registrationYearData.options : [];
+
+//----------Find models, registration months, and years data for the currently selected brand------------END--------------------------
+
+//---------Finally, the HTML code to be rendered and returned-------------------START----------------------------------
 
   return (
     <>
+      {/* our page header */}
       <h2>AutoWelt Coding Challenge: Cars API</h2>
       <div className="card">
         <p>Choose the brand of your car</p>
 
+        {/* drop down menu for the brands */}
         <select
           value={selectedBrand}
           onChange={(e) => setSelectedBrand(e.target.value)}
@@ -130,7 +139,8 @@ function App() {
             </option>
           ))}
         </select>
-
+        
+        {/* hidden drop down menu for the models, appears only when selecting the brand */}
         {availableModels.length > 0 && (
           <>
             <br />
@@ -153,6 +163,7 @@ function App() {
           </>
         )}
 
+        {/* hidden table for the registration month/year, appears only when selecting the brand */}
         {selectedModel && (
           <>
             <br />
@@ -166,11 +177,11 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                {/* Only show the first entry */}
+                {/* TODO: figure out how to change based on previous selection. Currently only shows the first option (for month and year) */}
                 {registrationMonths.slice(0, 1).map((month, index) => (
                   <tr key={index}>
                     <td>{month.label}</td> 
-                    <td>{registrationYears[index]?.label || "N/A"}</td> 
+                    <td>{registrationYears[index]?.label || "Error"}</td> 
                   </tr>
                 ))}
               </tbody>
@@ -178,11 +189,16 @@ function App() {
           </>
         )}
 
+        {/* the reset button */}
         <br />
         <button onClick={handleReset}>Reset</button>
       </div>
     </>
   );
+
+//---------Finally, the HTML code to be rendered and returned-------------------END----------------------------------
+
+
 }
 
 export default App;
